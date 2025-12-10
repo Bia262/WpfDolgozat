e19d8a40d01ca0f0eb71e1978e851fa48ec328ec
Bia262 <vargab4@kkszki.hu>
2025-12-10 11:23:58 +0100

    goodfinish

--- WpfDolgozat/MainWindow.xaml.cs
+++ WpfDolgozat/MainWindow.xaml.cs
@@ -1,65 +1,84 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
 
-namespace WpfDolgozat
+namespace dolgozatok
 {
+
     public class Dolgozat
     {
-        public String Nev {  get; set; }
+        public string Nev { get; set; }
         public int Eletkor { get; set; }
-        public int Pontszan { get; set; }
+        public int Pontszam { get; set; }
 
-        public Dolgozat(String nev, int eletkor, int pontszam)
+        public Dolgozat(string nev, int eletkor, int pontszam)
         {
             Nev = nev;
             Eletkor = eletkor;
-            Pontszan = pontszam;
+            Pontszam = pontszam;
         }
     }
     public partial class MainWindow : Window
     {
+
         public List<Dolgozat> dolgozatok = new List<Dolgozat>();
+
         public MainWindow()
         {
             InitializeComponent();
+
             var sorok = File.ReadAllLines("dolgozatok.txt").Skip(1);
             foreach (string s in sorok)
             {
-                string[] darabok = s.Split(";");
+                string[] darabok = s.Split(';');
                 string neve = darabok[0];
-                int eletkora = Convert.ToInt32(darabok[1]);
-                int pontszama = Convert.ToInt32(darabok[2]);
+                int eletkora = int.Parse(darabok[1]);
+                int pontszama = int.Parse(darabok[2]);
                 dolgozatok.Add(new Dolgozat(neve, eletkora, pontszama));
-                
             }
             dataGrid.ItemsSource = dolgozatok;
         }
-        private void hozzaadas (object sender, EventArgs e)
+
+        private void hozzaadas(object sender, RoutedEventArgs e)
         {
             int eletkora;
             int pontszama;
-            if(nev.Text.Length > 0 
-                && int.TryParse(eletkor.Text,out eletkora)
-                &&eletkora>6&& int.TryParse(pontszam.Text,out pontszama) 
-                && pontszama>=0
-                && pontszama <=100)
+            if (nev.Text.Length > 0
+                && int.TryParse(eletkor.Text, out eletkora)
+                && eletkora > 6
+                && int.TryParse(pontszam.Text, out pontszama)
+                && pontszama >= 0
+                && pontszama <= 100)
             {
-                dolgozatok.Add(new Dolgozat(nev.Text,eletkora,pontszama));
-                dataGrid.ItemsSource= dolgozatok;
+                dolgozatok.Add(new Dolgozat(nev.Text, eletkora, pontszama));
+                dataGrid.ItemsSource = dolgozatok;
                 dataGrid.Items.Refresh();
             }
             else
             {
-                MessageBox.Show("Hibás");
+                MessageBox.Show("Hibás adatbevitel!");
+            }
+        }
+
+        private void mentes(object sender, RoutedEventArgs e)
+        {
+            string fajlba = "";
+            foreach (var i in dolgozatok)
+            {
+                fajlba += i.Nev + ";" + i.Eletkor + ";" + i.Pontszam + "\n";
+            }
+            try
+            {
+                File.WriteAllText("dolgozatok.txt", fajlba);
+                MessageBox.Show($"Sikeres Mentés!", $"Infó", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show("Hiba a fájl mentése során: " + ae.Message);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show("Hiba a fájl mentése során: " + ioe.Message);
             }
         }
     }
